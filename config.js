dbd87cab3dc9005d629350db8f862b98a02ca57b
SosmithOsei <sosmithosei@gmail.com>
2025-06-04 11:50:09 +0000

    config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "IK~7clWwBTT#1NJPcqoaWgu-LXz3w6-WzcC0SlVfGonkIySlgEtKlAU",
+SESSION_ID: process.env.SESSION_ID || "FAISAL~CRMkBSKR#eu_0_PUYeEjh6CxM6TdtElIigtHHtXdDTyCGvo9vUJo",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
